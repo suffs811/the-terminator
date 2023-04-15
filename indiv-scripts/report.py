c4c89c99289da9c65aa4ee0ceaf4a78f54b65908
suffs811 <84633135+suffs811@users.noreply.github.com>
2023-04-15 12:19:00 -0400

    Update report.py
    Signed-off-by: suffs811 <84633135+suffs811@users.noreply.github.com>

--- indiv-scripts/report.py
+++ indiv-scripts/report.py
@@ -1,42 +1,45 @@
 #!/usr/bin/python3
 # author: suffs811
-# https://github.com/cysec11/scripts.git
-# purpose: automate common enumeration techniques 
-# using nmap, nikto, dirbuster, and enum4linux.
+# https://github.com/suffs811/the-terminator.git
+# purpose: script for creating report from terminator findings
 #
-# usage: python3 report.py -w <path_to_directory_list> <target_ip>
+# usage: python3 report.py -o <output_file_name>
 
 
 import os
 import argparse
-import time
-import re
 
 
-parser = argparse.ArgumentParser(description="script for making a report from pentest scripts\nusage: python3 report.py 10.0.0.1")
-parser.add_argument("ip", help="ip or domain to enumerate")
-parser.add_argument("-w", "--wordlist", help="specify wordlist for directory walking (gobuster)", required="True")
+parser = argparse.ArgumentParser(description="script for making a report from pentest scripts\npython3 report.py -o <output_file_name>")
+parser.add_argument("-o", "--output", help="specify name for report")
 args = parser.parse_args()
-ip = args.ip
-wordlist = args.wordlist
-
-scans = input("\n### Have you run enum/data_exfil/priv/pers.py scripts? (y/n): ")
-
-'''
--enum
-
--data_exfil
-
--priv
-
--pers
-
-'''
+output = args.output
 
 
+# create file and write contents to it
+def create(ip,output):
+   os.system("touch /terminator/report.txt")
+   os.system("echo '-+- Penetration Testing Report for {} -+-' >> /terminator/report.txt".format(ip))
+   os.system("echo '' >> /terminator/report.txt")
+   os.system("echo '+ + + Stage 1 - Enumeration + + +' >> /terminator/report.txt")
+   os.system("echo '' >> /terminator/report.txt")
+   os.system("cat /terminator/enum.txt >> /terminator/report.txt")
+   os.system("echo '' >> /terminator/report.txt")
+   os.system("echo '+ + + Stage 2 - Exploitation / Initial Shell + + +' >> /terminator/report.txt")
+   os.system("echo '' >> /terminator/report.txt")
+   os.system(" echo ' *** ADD YOUR EXPLOITION METHOD FOR THE INITAL SHELL HERE ***' >> /terminator/report.txt")
+   os.system("echo '' >> /terminator/report.txt")
+   os.system("echo '+ + + Stage 3 - Privilege Escalation + + +' >> /terminator/report.txt")
+   os.system("echo '' >> /terminator/report.txt")
+   os.system("cat /terminator/priv.txt >> /terminator/report.txt")
+   os.system("echo '' >> /terminator/report.txt")
+   os.system("echo '+ + + Stage 4 - Persistence and Data Exfiltration + + +' >> /terminator/report.txt")
+   os.system("cat /terminator/data_exfil.txt >> /terminator/report.txt")
+   os.system("echo '' >> /terminator/report.txt")
+   os.system("echo '' >> /terminator/report.txt")
+   os.system("echo '--- END OF REPORT ---' >> /terminator/report.txt")
+   os.system("mv /terminator/report.txt /terminator/{}".format(output))
+   print("### penetration test report for {} is ready at /terminator/{} ###\nplease add your method for gaining the initial shell in the '+ + + Stage 2 - Exploitation / Initial Shell + + +' section.\nall reference data for enumeration, privilege escalation, and persistence/data exfiltration are located in /terminator/ as enum.txt, priv.txt, and data_exfil.txt, respectively.".format(ip,output))
+   print("\n\n-+- {} has been terminated -+-".format(ip))
 
-if scans:
-	# call functions
-	pass
-else:
-	print("\n*** run enum/data_exfil/priv/pers.py scripts before running this report script... ***")
+create(ip,output)
