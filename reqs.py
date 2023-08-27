078fef8053d6573201e493c1e2853a4b4911f0f1
suffs811 <84633135+suffs811@users.noreply.github.com>
2023-08-27 12:03:46 -0400

    Create reqs.py
    Signed-off-by: suffs811 <84633135+suffs811@users.noreply.github.com>

---
+++ reqs.py
@@ -0,0 +1,9 @@
+import os
+
+# install dependencies
+os.system("sudo apt install nmap")
+os.system("sudo apt install nikto")
+os.system("sudo apt install gobuster")
+os.system("sudo apt install enum4linux")
+os.system("pip install python-docx")
+os.system("python3 -m pip install python-docx")
